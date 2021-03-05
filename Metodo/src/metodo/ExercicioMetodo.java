ed8ac84304cdacd11340cfc1d380f904e3516b34
MarcosGOFavaretto <marcos.favaretto@etec.sp.gov.br>
2021-03-05 12:38:11 -0300

    :sparkles:FEAT: Adicona os arquivos pertencentes ao projeto!
    Adicona os arquivos pertencentes ao projeto que foram criatos durantes as aulas

---
+++ Metodo/src/metodo/ExercicioMetodo.java
@@ -0,0 +1,26 @@
+/*
+ * Programa Exercício - Método
+ * Autor: Marcos Gabriel De Olveira Favaretto
+ * Aula de DS - 01/08/2019
+ */
+package metodo;
+
+/**
+ *
+ * @author ETEC ALUNO
+ */
+public class ExercicioMetodo {//Início da classe
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {//Início do método principal
+        falar();
+        System.out.println("Hello!");
+    }//Fim do método principal
+
+    public static void falar() {
+        System.out.println("Boa Noite! Meu nome é Marcos.");
+    }
+
+}//Fim da classe
