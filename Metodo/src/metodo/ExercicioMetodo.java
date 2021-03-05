bb03e94c91003d09c46a95e831f0fecaa31c8fb0
MarcosGOFavaretto <marcos.favaretto@etec.sp.gov.br>
2021-03-05 12:46:26 -0300

    :art:STYLE: Remove as anotações da aula e otimiza comentários! Remove as anotações da aula e otimiza comentários da classe principal do programa fornecendo informações como o nome do projeto, o autor e a data

--- Metodo/src/metodo/ExercicioMetodo.java
+++ Metodo/src/metodo/ExercicioMetodo.java
@@ -1,26 +1,18 @@
-/*
- * Programa Exercício - Método
+/**
+ * Programa: Método
  * Autor: Marcos Gabriel De Olveira Favaretto
- * Aula de DS - 01/08/2019
+ * Data: 01 de Agosto de 2019.
  */
 package metodo;
 
-/**
- *
- * @author ETEC ALUNO
- */
-public class ExercicioMetodo {//Início da classe
+public class ExercicioMetodo {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {//Início do método principal
+    public static void main(String[] args) {
         falar();
         System.out.println("Hello!");
-    }//Fim do método principal
+    }
 
     public static void falar() {
         System.out.println("Boa Noite! Meu nome é Marcos.");
     }
-
-}//Fim da classe
+}
